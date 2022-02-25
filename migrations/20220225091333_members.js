a8509ebd5d7afebe965ecffc910fc7e0bef7a27b
James Mwantisi <jamesmwantisi@gmail.com>
2022-02-25 16:00:18 +0200

    refactor

--- migrations/20220225091333_members.js
+++ migrations/20220225091333_members.js
@@ -6,6 +6,10 @@ exports.up = function (knex) {
 		members.string('lastname')
 		members.string('phone_number')
 		members.string('address')
+		members.integer('member_type_id').unsigned()
+		members.foreign('member_type_id').references('id').inTable('member_types');
+		members.integer('team_id').unsigned()
+		members.foreign('team_id').references('id').inTable('teams');
 		members.integer('enabled').notNullable().defaultTo(0)
 		members.integer('void').notNullable().defaultTo(0)
 		members.timestamps();
