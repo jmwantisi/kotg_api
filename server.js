0d97723de3f444607326dd2bd76261337e8b6055
James Mwantisi <jamesmwantisi@gmail.com>
2022-02-25 13:46:36 +0200

    moved body parser logic to app.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 import dotenv from 'dotenv'
 import app from './app'
-import bodyParser from 'body-parser';
 
 const PORT = process.env.SERVER_PORT
 
 const FILE_SIZE_LIMIT = process.env.FILE_SIZE_LIMIT
 
 dotenv.config();
-app.use(bodyParser.json({
-	limit: FILE_SIZE_LIMIT
-}));
 
-app.listen(PORT, () => {
+
+app.listen(5000, () => {
 	console.log(`app is listening to port ${5000}`);
 })
