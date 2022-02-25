ad293c02741d616d4da247f495dc9ac38ee38264
James Mwantisi <jamesmwantisi@gmail.com>
2022-02-25 13:45:55 +0200

    configured teams route

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors'
+import teamsRoutes from './routes/teams_route';
+import bodyParser from 'body-parser';
 import { ExtractJwt, passport } from 'passport-jwt'
 
 const app = express();
 app.use(cors())
 app.options('*', cors())
+
+app.use(bodyParser.json({
+	limit: 1000000000000
+}));
+
+app.use('/teams', teamsRoutes);
 
 module.exports = app;
 
